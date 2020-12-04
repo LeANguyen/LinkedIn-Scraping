fdb91c4d7bde68963c83acdf562c0c690f9cee3f
LeANguyen <scorcher159@gmail.com>
2020-12-04 13:19:06 +0700

    first commit and create basic ./components and ./screens

---
+++ src/screens/MultipleScrapingScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MultipleScrapingScreen = () => {
+  return <div></div>;
+};
+
+export default MultipleScrapingScreen;
---
+++ src/screens/SingleScrapingScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleScrapingScreen = () => {
+  return <div></div>;
+};
+
+export default SingleScrapingScreen;
