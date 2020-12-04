9bceb8b1ab121e4980b58676840f00fa2c0a5034
LeANguyen <scorcher159@gmail.com>
2020-12-04 14:26:15 +0700

    fix <p> App.js typo

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit the MAN FUCK YOU GITHUB WHy you so complicated AND YOU GIVE ME
-          SEIZURE AND I am depressed and I will nuke your HQ
+          Edit the MAN FUCK YOU GITHUB Why are you so complicated AND YOU GIVE
+          ME SEIZURE AND I am depressed and I will nuke your HQ
           <code>src/App.js</code> and save to reload.
         </p>
         <a
