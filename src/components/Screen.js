f6dcef020753d9c341fb4e4838951d08ef598e0f
LeANguyen <scorcher159@gmail.com>
2020-12-04 22:45:57 +0700

    stash

---
+++ src/components/Screen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+const Screen = ({ children }) => {
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
+      <Header></Header>
+      {children}
+      {/* <Footer></Footer> */}
+    </div>
+  );
+};
+
+export default Screen;
